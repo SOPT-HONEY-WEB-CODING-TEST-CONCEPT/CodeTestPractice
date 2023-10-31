55fa8640394f9023f6f5c25c8657113781a83bb0
jungwoo3490 <jungwoo3490@naver.com>
2023-11-01 02:18:49 +0900

    1주차 과제 1 - K번째수 (수정)

--- 정우/week1/K번째수.js
+++ 정우/week1/K번째수.js
@@ -1,7 +1,7 @@
 function solution(array, commands) {
     var answer = [];
     commands.forEach((item) => {
-        sliceArray = array.slice(item[0] - 1, item[1]);
+        let sliceArray = array.slice(item[0] - 1, item[1]);
         sliceArray.sort((a, b) => a - b);
         answer.push(sliceArray[item[2] - 1]);
     })
