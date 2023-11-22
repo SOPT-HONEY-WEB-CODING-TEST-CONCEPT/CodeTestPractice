23ca75333d33de82f42559c661bf1fcb85a88a17
jungwoo3490 <jungwoo3490@naver.com>
2023-11-22 10:40:38 +0900

    4주차 과제 1 - 완주하지 못한 선수

---
+++ 정우/week4/완주하지 못한 선수.js
@@ -0,0 +1,12 @@
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i += 1) {
+    if (participant[i] !== completion[i]) {
+      return participant[i];
+    } else {
+      continue;
+    }
+  }
+}
