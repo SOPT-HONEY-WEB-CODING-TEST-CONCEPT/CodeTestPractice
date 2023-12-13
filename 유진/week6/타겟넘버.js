944de11cd2e44d6521cc043cf2de60b6ab044c09
nayujin-dev <00skdbwls@gmail.com>
2023-12-13 18:51:17 +0900

    [week6] dfs, bfs

---
+++ 유진/week6/타겟넘버.js
@@ -0,0 +1,13 @@
+function solution(numbers, target) {
+  var answer = 0;
+  function dfs(index, tot) {
+    if (index === numbers.length) {
+      if (tot === target) ++answer;
+      return;
+    }
+    dfs(index + 1, tot + numbers[index]);
+    dfs(index + 1, tot - numbers[index]);
+  }
+  dfs(0, 0);
+  return answer;
+}
